e839c704ba5cbc17192c42cbca0f73c8211ad4a5
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-10-16 23:40:31 +0900

    Initial Commit

---
+++ ghsample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ghsample
+//
+//  Created by nagata on 2017/10/16.
+//  Copyright © 2017年 nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
